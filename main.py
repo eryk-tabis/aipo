610e75b87de8d8f379c316a0d2f2999a00c2348a
Eryk Tabis <s222765@student.uek.krakow.pl>
2023-12-18 19:29:49 +0100

    fix: fix mistake with existing same classes

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.locations.world import World
+def main():
+    world = World()
+    setup(world)
+    player = create_character()
+    while True:
+        pass
+
+def create_character():
+    """Create the player character"""
+    pass
+def setup(obj: World):
+    """Setup the world with locations and items"""
+    pass
+
+if __name__ == "__main__":
+    main()
