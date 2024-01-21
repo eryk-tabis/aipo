2bf4c5d8dbc69127fcd2fd0c27adf88c6c0e3b9d
Łukasz <s221540@student.uek.krakow.pl>
2024-01-21 15:11:59 +0100

    fix3

--- src/items/apple.py
+++ src/items/apple.py
@@ -5,7 +5,7 @@ class Apple(Item):
         super(Apple,self).__init__
         self.name = "Jabłko"
         self.description = "Pyszne słodkie i soczyste"
-        self.type = "leczenie"
+        self.type = "Healing"
         self.use_value = Item("Jabłko","Pyszne słodkie i soczyste","leczenie",5,1) # This is the value that the item will do when used
         self.amount = 1
     
